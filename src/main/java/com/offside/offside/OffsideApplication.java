22d26cb6cb02fdb5f07209439913a65aafaae190
오정환 <ohjeonghwan@ojeonghwan-ui-MacBookAir.local>
2021-10-18 20:00:23 +0900

    초기 프로젝트

---
+++ src/main/java/com/offside/offside/OffsideApplication.java
@@ -0,0 +1,13 @@
+package com.offside.offside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffsideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offside/offside/OffsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offside.offside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
