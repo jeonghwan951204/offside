2ff342a0499d37ecd9e3ce6540a197ebef0e8289
jhy950210 <jhyy1995210@gamil.com>
2021-11-08 23:09:41 +0900

    build : add postgresql

--- src/main/java/com/offside/offside/vo/Player.java
+++ src/main/java/com/offside/offside/vo/Player.java
@@ -1,5 +1,16 @@
 package com.offside.offside.vo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Player {
 
+    @Id @GeneratedValue
+    @Column(name = "player_id")
+    private Long id;
+
+    private String name;
 }
